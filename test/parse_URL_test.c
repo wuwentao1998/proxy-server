d8149b29a6015e63a7025f1ad4b30212e15a1371
wuwentao1998 <winter_wu@sjtu.edu.cn>
2019-06-12 20:54:37 +0800

    update unit test

---
+++ test/parse_URL_test.c
@@ -0,0 +1,97 @@
+#include "../src/http.h"
+#include <assert.h>
+
+void test_1()
+{
+    char host[MAXLINE];
+    char URI[MAXLINE];
+    int port;
+
+    char URL[MAXLINE] = "www.baidu.com/index.html";
+    parse_URL(URL, URI, host, &port);
+
+    assert(port == 80);
+    assert(strcmp(host, "www.baidu.com") == 0);
+    assert(strcmp(URI, "/index.html") == 0);
+}
+
+void test_2()
+{
+    char host[MAXLINE];
+    char URI[MAXLINE];
+    int port;
+
+    char URL[MAXLINE] = "http://www.baidu.com/index.html";
+    parse_URL(URL, URI, host, &port);
+
+    assert(port == 80);
+    assert(strcmp(host, "www.baidu.com") == 0);
+    assert(strcmp(URI, "/index.html") == 0);
+}
+
+void test_3()
+{
+    char host[MAXLINE];
+    char URI[MAXLINE];
+    int port;
+
+    char URL[MAXLINE] = "https://www.baidu.com/index.html";
+    parse_URL(URL, URI, host, &port);
+
+    assert(port == 80);
+    assert(strcmp(host, "www.baidu.com") == 0);
+    assert(strcmp(URI, "/index.html") == 0);
+}
+
+void test_4()
+{
+    char host[MAXLINE];
+    char URI[MAXLINE];
+    int port;
+
+    char URL[MAXLINE] = "https://www.baidu.com:8080/dir1/dir2/main";
+    parse_URL(URL, URI, host, &port);
+
+    assert(port == 8080);
+    assert(strcmp(host, "www.baidu.com") == 0);
+    assert(strcmp(URI, "/dir1/dir2/main") == 0);
+}
+
+void test_5()
+{
+    char host[MAXLINE];
+    char URI[MAXLINE];
+    int port;
+
+    char URL[MAXLINE] = "https://www.baidu.com:8080";
+    parse_URL(URL, URI, host, &port);
+
+    assert(port == 8080);
+    assert(strcmp(host, "www.baidu.com") == 0);
+    assert(strcmp(URI, "/") == 0);
+}
+
+void test_6()
+{
+    char host[MAXLINE];
+    char URI[MAXLINE];
+    int port;
+
+    char URL[MAXLINE] = "www.baidu.com";
+    parse_URL(URL, URI, host, &port);
+
+    assert(port == 80);
+    assert(strcmp(host, "www.baidu.com") == 0);
+    assert(strcmp(URI, "/") == 0);
+}
+
+
+int main(void)
+{
+    test_1();
+    test_2();
+    test_3();
+    test_4();
+    test_5();
+    test_6();
+}
