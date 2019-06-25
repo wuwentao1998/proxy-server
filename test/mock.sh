b88560b65f77f23d970e2f06f047c55488d3c698
武文韬 <wuwentao.1024@bytedance.com>
2019-06-25 22:44:08 +0800

    fix: wrong judgment in mock.sh
    if the file is empty -s filename is true, not false

--- test/mock.sh
+++ test/mock.sh
@@ -13,9 +13,11 @@ curl "127.0.0.1:8080" > a.out
 curl -x 127.0.0.1:8000 "127.0.0.1:8080" > b.out
 diff a.out b.out > c.out
 
-if [ ! -s c.out ]
+if [ -s c.out ]
 then
     echo "=====mock test failed====="
+    echo "content in diff file:"
+    cat c.out
 else
     echo "=====mock test done====="
 fi
@@ -24,4 +26,4 @@ rm -f proxy a.out b.out c.out
 cd ${CUR_DIR}
 rm -f nohup.out
 ps aux | grep "python3 -m http.server 8080" | grep -v "grep" | awk '{print $2}' | xargs kill -9
-ps aux | grep "./proxy 8000&" | grep -v "grep" | awk '{print $2}' | xargs kill -9
+ps aux | grep "./proxy 8000" | grep -v "grep" | awk '{print $2}' | xargs kill -9
