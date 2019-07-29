9540f2f3fc5c4476768cff82380e971fb6ddfcda
wuwentao1998 <winter_wu@sjtu.edu.cn>
2019-07-29 13:22:37 +0800

    chore: modify the startup script

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM gcc:latest
 MAINTAINER wuwentao "winter_wu@sjtu.edu.cn"
 
-COPY src /proxy-server
+COPY . /proxy-server
 WORKDIR /proxy-server
-RUN make; make clean
+RUN cd src/; make; make clean; cd ..; mkdir output; cp ./src/proxy output; cp -r log ouput; cd output/
 
 EXPOSE 8000
--- run.sh
+++ run.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
 VERSION=v0.2
-PORT=$1
+METHOD=$1
+PORT=$2
 NAME=proxy
 
-# build
-docker build -t="${NAME}:${VERSION}" .
-# run
-docker run --net host -itd --name ${NAME} ${NAME}:${VERSION} bash -c "./${NAME} ${PORT}"
+if [ "${METHOD}" == "docker" ]; then
 
+    # build
+    docker build -t="${NAME}:${VERSION}" .
+    # run
+    docker run --net host -itd --name ${NAME} ${NAME}:${VERSION} bash -c "./${NAME} ${PORT}"
+
+elif [ "${METHOD}" == "shell" ]; then
+
+    cd src/
+    make; make clean
+    cd ..
+    mkdir output
+    cp ./src/proxy output
+    cp -r log ouput
+    cd output/
+    ./proxy ${PORT}&
+
+elif [ "X${METHOD}" == "X" ]; then
+    echo "Lack of parameter！"
+    exit 1
+else
+    echo "Illegal parameter！"
+    exit 1
+fi
